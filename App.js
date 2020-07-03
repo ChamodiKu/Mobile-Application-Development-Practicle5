b138ab83d9b4f5473de61a55139b27fa56fe464e
ChamodiKu <kulathungachamodi@gmail.com>
2020-07-03 17:24:52 +0530

    Add new project to git

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TextInput, AppRegistry} from 'react-native';
+//*********//
+//import Constants from 'expo-constants';
+
+// You can import from local files
+//import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+//import { Card } from 'react-native-paper';
+//***********//
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  const [name, setName] =useState('');
+  const [age, setAge] =useState('');
+   
+      return (
+        <View style={styles.container}>
+        
+          <Text>Enter your name:</Text>
+          <TextInput 
+            style={styles.inputBox}
+            onChangeText={(val) => setName(val)}
+          /> 
+        
+          <Text>Enter your age:</Text>
+          <TextInput 
+            style={styles.inputBox}
+            keyboardType = 'numeric' 
+            onChangeText={(val)=> setAge(val)}
+          /> 
+        
+        <Text>Hello, {name}. Your age is {age}
+        </Text>
+      
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    alignItems:'center', 
+    justifyContent: 'center'
   },
+  
+  inputBox: {
+    height: 40,
+    width: 100,
+    
+    borderColor: 'blue',
+    borderWidth:1
+  } 
 });
